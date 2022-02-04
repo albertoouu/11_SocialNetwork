09c8ddac780d9f0bb60b8fd71288ca9522f7d05e
Alberto Gutierrez <albertoouu@gmail.com>
2022-02-04 02:13:01 -0600

    add create post

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -2,18 +2,19 @@ import React from "react";
 import { Stack, Container, Row, Col, Button } from "react-bootstrap";
 
 const NewsFeed = ({ arrayPosts }) => {
+  const sortedarray = arrayPosts.slice().sort((a, b) => b.Date - a.Date);
   return (
     <Container>
       <Stack>
-        {arrayPosts.map((obj) => {
+        {sortedarray.map((obj) => {
           return (
-            <>
+            <div key={obj.Date}>
               <h1>{obj.title}</h1>
               <h2>{obj.subheader}</h2>
               <h3>{obj.author}</h3>
               <p>{obj.content}</p>
               <p>{obj.cardcontent}</p>
-            </>
+            </div>
           );
         })}
       </Stack>
